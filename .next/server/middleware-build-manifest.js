1baa6b52c2318d05c066791264336d78879e9e8e
PasandiRanga <140192434+PasandiRanga@users.noreply.github.com>
2025-06-12 22:00:53 +0530

    Updated react-loadable-manifest.json

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -1,4 +1,4 @@
-{
+globalThis.__BUILD_MANIFEST = {
   "polyfillFiles": [
     "static/chunks/polyfills.js"
   ],
@@ -6,10 +6,7 @@
     "static/chunks/react-refresh.js"
   ],
   "ampDevFiles": [],
-  "lowPriorityFiles": [
-    "static/development/_buildManifest.js",
-    "static/development/_ssgManifest.js"
-  ],
+  "lowPriorityFiles": [],
   "rootMainFiles": [],
   "rootMainFilesTree": {},
   "pages": {
@@ -25,4 +22,9 @@
     ]
   },
   "ampFirstPages": []
-}
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+,"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js",
+
+];
